1c762488ed17a708051169e91e9e938f58745b7d
RBord <rostyslavbord@gmail.com>
2021-10-06 21:37:55 +0300

    done with register

---
+++ controllers/auth/login.js
@@ -0,0 +1,7 @@
+const { User } = require('../../models');
+
+const login = async (req, res) => {
+    
+};
+
+module.exports = login;
--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const { Schema, model } = require('mongoose');
 const Joi = require('joi');
+const bcrypt = require('bcryptjs');
 
 const joiSchema = Joi.object({
     email: Joi.string().required(),
@@ -17,6 +18,10 @@ const userSchema = Schema({
         minlength: 6
     }
 }, { versionKey: false, timestamps: true });
+
+userSchema.methods.setPassword = function (password) {
+    this.password = bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+}
 
 const User = model('user', userSchema);
 
