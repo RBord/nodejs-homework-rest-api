faca91294a529332af6cd5a285b5ce473d75ed9a
RBord <rostyslavbord@gmail.com>
2021-10-07 14:42:08 +0300

    done login operation

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -1,7 +1,27 @@
 const { User } = require('../../models');
+const { NotFound, BadRequest } = require('http-errors');
+const bcrypt = require('bcryptjs');
 
 const login = async (req, res) => {
-    
+    const { email, password } = req.body;
+    const user = await User.findOne({ email }, '_id email password');
+    if (!user || !user.comparePassword(password)) {
+        throw new BadRequest('Invalid email or password');
+    }
+    // if (!user) {
+    //     throw new NotFound(`Email ${email} not found`);
+    // }
+    // if (!user.comparePassword(password)) {
+    //     throw new BadRequest('Invalid password');
+    // }
+    const token = 'sdlgsdgsdg.sdgsdgsdgsdg.sgsdgsgsdgrg';
+    res.json({
+        status: 'succes',
+        code: 200,
+        data: {
+            token
+        }
+    })
 };
 
 module.exports = login;
