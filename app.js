98a5bbae4577ef8b4b2d3a68737e330c94f88085
RBord <rostyslavbord@gmail.com>
2021-10-14 15:27:45 +0300

    in process multer hw05

--- app.js
+++ app.js
@@ -1,37 +1,74 @@
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
-// const multer = require('multer')
-// const path = require('path')
-
-// const tempDir = path.join(__dirname, 'temp')
-
-// const uploadConfig = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, tempDir)
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, file.originalname)
-//   },
-//   limits: {
-//     fileSize: 2048,
-//   },
-// })
-
-// const upload = multer({
-//   storage: uploadConfig,
-// })
+const multer = require('multer')
+const path = require('path')
+const fs = require('fs/promises')
 
 const contactsRouter = require('./routes/api/contacts')
 const authRouter = require('./routes/api/auth')
 
 const app = express()
 
+const tempDir = path.join(__dirname, 'temp')
+const avatarsDir = path.join(__dirname, 'public')
+
+const uploadConfig = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, tempDir)
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname)
+  },
+  limits: {
+    fileSize: 2048,
+  },
+})
+
+const uploadMiddleware = multer({
+  storage: uploadConfig,
+})
+
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
 app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
+
+const avatars = []
+
+app.post(
+  '/api/auth/avatars',
+  uploadMiddleware.single('avatar'),
+  async (req, res) => {
+    const { originalname, path: tempName } = req.file
+    const fileName = path.join(avatarsDir, 'avatars', originalname)
+    try {
+      await fs.rename(tempName, fileName)
+      const avatar = path.join('public/avatars', originalname)
+      const newAvatar = { ...req.body, avatar }
+      avatars.push(newAvatar)
+      res.status(201).json({
+        status: 'success',
+        code: 201,
+        data: {
+          result: newAvatar,
+        },
+      })
+    } catch (error) {
+      await fs.unlink(tempName)
+    }
+  },
+)
+app.get('/api/auth/avatars', (req, res) => {
+  res.json({
+    status: 'succes',
+    code: 200,
+    data: {
+      avatars,
+    },
+  })
+})
 
 app.use('/api/contacts', contactsRouter)
 app.use('/api/auth', authRouter)
